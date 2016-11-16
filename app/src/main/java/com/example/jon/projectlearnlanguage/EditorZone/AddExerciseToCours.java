e8871b373785ef4dc566deafb8f1aedb09dd50bd
Jon <joaquim.jonathan@bluewin.ch>
2016-11-16 12:37:22 +0100

    Création de l'EditorZone

---
+++ app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/AddExerciseToCours.java
@@ -0,0 +1,15 @@
+package com.example.jon.projectlearnlanguage.EditorZone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jon.projectlearnlanguage.R;
+
+public class AddExerciseToCours extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_exercise_to_cours);
+    }
+}
---
+++ app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/UserView.java
@@ -0,0 +1,15 @@
+package com.example.jon.projectlearnlanguage.EditorZone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jon.projectlearnlanguage.R;
+
+public class UserView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_view);
+    }
+}
