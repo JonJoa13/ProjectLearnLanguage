0bcb422d1aecfa05aaf522158895a47b1685c388
TheoP <pillet.theodore@icloud.com>
2016-11-25 14:38:02 +0100

    - java : createCours, createExercise

--- app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/CreateCours.java
+++ app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/CreateCours.java
@@ -1,11 +1,19 @@
 package com.example.jon.projectlearnlanguage.EditorZone;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.EditText;
 
 import com.example.jon.projectlearnlanguage.R;
+
+import java.io.Reader;
+
+import db.object.ReaderContract;
+import db.object.SQLiteHelper;
 
 public class CreateCours extends AppCompatActivity {
 
@@ -24,6 +32,30 @@ public class CreateCours extends AppCompatActivity {
     }
 
     public void onClickCreateCours(View w){
+        Intent intent = new Intent(CreateCours.this, CoursView.class);
+        startActivity(intent);
+    }
+
+    public void createCours(){
+        SQLiteHelper mDbHelper = new SQLiteHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        EditText editTextCours,
+                editTextLevel;
+
+        editTextCours = (EditText)findViewById(R.id.et_cours);
+        editTextLevel = (EditText)findViewById(R.id.et_level);
+
+        String cours = editTextCours.getText().toString();
+        String level = editTextLevel.getText().toString();
+
+        ContentValues values = new ContentValues();
+        values.put(ReaderContract.CoursEntry.KEY_TITRE, cours);
+        values.put(ReaderContract.CoursEntry.KEY_LEVEL, level);
+
+        db.insert(ReaderContract.CoursEntry.TABLE_COURS, ReaderContract.CoursEntry.KEY_NULLABLE, values);
+        db.close();
+
         Intent intent = new Intent(CreateCours.this, CoursView.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/CreateExercise.java
+++ app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/CreateExercise.java
@@ -1,11 +1,17 @@
 package com.example.jon.projectlearnlanguage.EditorZone;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.jon.projectlearnlanguage.R;
+
+import db.object.ReaderContract;
+import db.object.SQLiteHelper;
 
 public class CreateExercise extends AppCompatActivity {
 
@@ -24,6 +30,39 @@ public class CreateExercise extends AppCompatActivity {
     }
 
     public void onClickCreateExercise(View w){
+        Intent intent = new Intent(CreateExercise.this, ExerciseView.class);
+        startActivity(intent);
+    }
+
+    public void createExercise(){
+        SQLiteHelper mDbHelper = new SQLiteHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        EditText editTextTitleEx,
+                editTextType,
+                editTextContent,
+                editTextSolution;
+
+        editTextTitleEx = (EditText)findViewById(R.id.et_titleEx);
+        editTextType = (EditText)findViewById(R.id.et_type);
+        editTextContent = (EditText)findViewById(R.id.et_content);
+        editTextSolution = (EditText)findViewById(R.id.et_solution);
+
+        String titre = editTextTitleEx.getText().toString();
+        String type = editTextType.getText().toString();
+        String content = editTextContent.getText().toString();
+        String solution = editTextSolution.getText().toString();
+
+
+        ContentValues values = new ContentValues();
+        values.put(ReaderContract.ExerciseEntry.KEY_TITRE, titre);
+        values.put(ReaderContract.ExerciseEntry.KEY_TYPE, type);
+        values.put(ReaderContract.ExerciseEntry.KEY_DONNEE, content);
+        values.put(ReaderContract.ExerciseEntry.KEY_SOLUTION, solution);
+
+        db.insert(ReaderContract.ExerciseEntry.TABLE_EXERCISE, ReaderContract.ExerciseEntry.KEY_NULLABLE, values);
+        db.close();
+
         Intent intent = new Intent(CreateExercise.this, ExerciseView.class);
         startActivity(intent);
     }
