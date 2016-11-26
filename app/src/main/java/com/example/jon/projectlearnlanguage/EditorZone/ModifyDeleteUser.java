9e9e2c4f75f1a7809267aff1d0986f1ac84af8dc
TheoP <pillet.theodore@icloud.com>
2016-11-26 16:32:12 +0100

    - Delete (Cancel OK - DELETE NOK - DialogAlert OK)

--- app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/ModifyDeleteUser.java
+++ app/src/main/java/com/example/jon/projectlearnlanguage/EditorZone/ModifyDeleteUser.java
@@ -1,61 +1,27 @@
 package com.example.jon.projectlearnlanguage.EditorZone;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jon.projectlearnlanguage.R;
 
-import db.object.ReaderContract;
-import db.object.SQLiteHelper;
-import db.object.adapter.UserDataSource;
 import db.object.object.User;
 
 public class ModifyDeleteUser extends AppCompatActivity {
-    Bundle bundle;
-    Integer userId;
-    EditText editTextName,
-             editTextFirstname,
-             editTextEmail,
-             editTextMDP;
-    UserDataSource uds;
-    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
         setContentView(R.layout.activity_modify_delete_user);
-
-        uds = new UserDataSource(this);
-
-        if(savedInstanceState == null){
-            bundle = getIntent().getExtras();
-            if(bundle == null){
-                userId = null;
-            } else {
-                userId = bundle.getInt("idUser");
-            }
-        }else{
-            userId = (int) savedInstanceState.getSerializable("idUser");
-        }
-
-
-        editTextName = (EditText) findViewById(R.id.et_name);
-        editTextFirstname = (EditText) findViewById(R.id.et_firstname);
-        editTextEmail = (EditText) findViewById(R.id.et_email);
-        editTextMDP = (EditText) findViewById(R.id.et_mdp);
-
-        user = uds.getUserById(userId);
-
-        editTextName.setText(user.getName());
-        editTextFirstname.setText(user.getFirstname());
-        editTextEmail.setText(user.getEmail());
-        editTextMDP.setText(user.getMdp());
     }
 
     public void onClickBackToUserView(View w){
@@ -70,8 +36,39 @@ public class ModifyDeleteUser extends AppCompatActivity {
 
     public void onClickDeleteUser(View w){
         Intent intent = new Intent(ModifyDeleteUser.this,UserView.class);
-        startActivity(intent);
-    }
+        AlertDialog.Builder alert = new AlertDialog.Builder (this);
+        // Setting Dialog Title
+        alert.setTitle(R.string.da_titel);
 
+        // Setting Dialog Message
+        alert.setMessage(R.string.da_message);
+
+        // Setting Icon to Dialog
+        alert.setIcon(R.drawable.exit_icon);
+
+        // Setting Positive "Yes" Button
+        alert.setPositiveButton(R.string.da_btn_1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                // User pressed YES button. Write Logic Here
+                Toast.makeText(getApplicationContext(), "You clicked on YES",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Setting Neutral "Cancel" Button
+        alert.setNeutralButton(R.string.da_btn_2, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(ModifyDeleteUser.this,UserView.class);
+                startActivity(intent);
+                // User pressed Cancel button. Write Logic Here
+                Toast.makeText(getApplicationContext(), "You clicked on Cancel",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Showing Alert Message
+        alert.show();
+    }
 
 }
